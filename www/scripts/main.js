72b4acf283c744924fdd49beb36b89ae69b287dd
Michiel van Oosten <mail@axion5.net>
2014-10-14 03:27:29 +0200

    Sync Tue Oct 14 2014 03:27:29 GMT+0200 (CEST)

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -4,9 +4,9 @@ console.log('Hello from a node module!!!');
 // vouchdb.connect('http://couch.local');
 vouchdb.connect('http://localhost:5984');
 
-var callback = makeUUID();
-listenForMsgs(callback);
-console.log('Callback key: ', callback);
+var from = makeUUID();
+listenForMsgs(from);
+console.log('From key: ', from);
 
 // vouchdb.info().when(
 //     function(data) {
@@ -33,10 +33,10 @@ console.log('Callback key: ', callback);
 // }, 0, { include_docs: true  }, 'reception');
 
 
-function listenForMsgs(callback) {
+function listenForMsgs(from) {
     console.log('listening for public messages..');
     var options =
-        callback ? { include_docs: true, filter: 'app/bla', callback:callback } :
+        from ? { include_docs: true, filter: 'cape/from', from:from } :
     { inclue_docs: true };
     vouchdb.dbChanges(function(change) {
         change.results.forEach(function(result) {
@@ -52,7 +52,7 @@ if (location.pathname == '/confirm.html' &&
     console.log(uuid);
     vouchdb.docSave({ msg: 'confirm',
                       uuid: uuid,
-                      callback: callback
+                      from: from
                     }, 'reception');
 }
 
@@ -71,7 +71,7 @@ var cliks = $("#forgotpwd").asEventStream("click");
 cliks.onValue(function() {
     vouchdb.docSave({ msg:'forgotpwd',
                       usernameOrPassword: $("#username")[0].value,
-                      callback: callback
+                      from: from
                     }, 'reception')
         .when(
             function(data) {
@@ -103,7 +103,7 @@ cliks.onValue(function() {
                       username: $("#username")[0].value,
                       email: $("#email")[0].value,
                       pwd: $("#password")[0].value,
-                      callback: callback
+                      from: from
                     }, 'reception')
         .when(
             function(data) {
@@ -121,7 +121,7 @@ cliks.onValue(function() {
     vouchdb.docSave({ msg:'resetpwd',
                       pwd: $("#password1")[0].value,
                       uuid: location.search.slice(10),
-                      callback: callback
+                      from: from
                     }, 'reception')
         .when(
             function(data) {
